24dbb331718898c65ed1dfbf6640d409856ff259
Pavelliss <pav-kovrizhko@mail.ru>
2019-03-09 22:52:44 +0300

    Закончена верстка оставшихся блоков страницы

--- js/popup.js
+++ js/popup.js
@@ -6,20 +6,26 @@ var popupMap=document.querySelector(".popup-map");
 var map=document.querySelector(".map");
 var closeMap=document.querySelector(".map-close");
 
-/*var popupCotalog=document.querySelector(".popup-cotalog");
-var buy=document.querySelector(".button-buy");
-var buttonCatalog=document.querySelector(".button-catalog");*/
+var popupCotalog=document.querySelector(".popup-cotalog");
+var buy = document.querySelectorAll('.button-buy'),
+    index, buttonBuy; //Купить
+var buttonCatalog=document.querySelector(".button-catalog"); //Продолжить покупки
 
+//Обратная связь
+if(buttonContact){
 buttonContact.addEventListener("click",function(evt){
     evt.preventDefault();
     popup.classList.add("popup-show");
     userName.focus();
 });
+};
 
+if(closeButton){
 closeButton.addEventListener("click",function(evt){
     evt.preventDefault();
     popup.classList.remove("popup-show");
 });
+};
 
 window.addEventListener("keydown", function(evt){
     if(evt.keyCode===27){
@@ -30,16 +36,21 @@ window.addEventListener("keydown", function(evt){
     }
 });
 
+//Карта
 
+if(map){
 map.addEventListener("click",function(evt){
     evt.preventDefault();
     popupMap.classList.add("popup-show");
 });
+};
 
+if(closeMap){
 closeMap.addEventListener("click",function(evt){
     evt.preventDefault();
     popupMap.classList.remove("popup-show");
 });
+};
 
 window.addEventListener("keydown", function(evt){
     if(evt.keyCode===27){
@@ -50,7 +61,21 @@ window.addEventListener("keydown", function(evt){
     }
 });
 
-/*buy.addEventListener("click", function(evt){
-    evt.preventDefault();
-    popupCotalog.classList.add("popup-show");
-});*/
+//Кнопки купить
+
+if (buy){
+    for (index = 0; index < buy.length; index++) {
+    buttonBuy = buy[index];
+    buttonBuy.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        popupCotalog.classList.add("popup-show");
+    }); }
+};
+
+
+if(buttonCatalog){
+    buttonCatalog.addEventListener("click",function(evt){
+        evt.preventDefault();
+        popupCotalog.classList.remove("popup-show");
+    });
+};
